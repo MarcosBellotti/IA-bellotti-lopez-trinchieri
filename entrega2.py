33339dd9493eee23ef2b2416335b3667d4e849d1
Ignacio <ignacioelopez04@gmail.com>
2023-10-31 11:56:22 -0300

    Hicimos un par de restricciones

--- entrega2.py
+++ entrega2.py
@@ -7,6 +7,16 @@ from simpleai.search import (
 )
 from itertools import combinations
 
+FORMAS = {
+    'L': ((0,0), (1,0), (1,1)),
+    'T': ((0,0), (0,1), (0,2), (1,1)),
+    'O': ((0,0), (0,1), (1,1), (1,0)),
+    'I': ((0,0), (1,0), (2,0)),
+    '-': ((0,0), (0,1), (0,2)),
+    'Z': ((0,0), (0,1), (1,1), (1,2)),
+    '.': ((0,0)),
+}
+
 def armar_tablero(filas, columnas, pisos, salida, piezas, pieza_sacar):
     
     variables = [ pieza[0] for pieza in piezas]
@@ -15,30 +25,75 @@ def armar_tablero(filas, columnas, pisos, salida, piezas, pieza_sacar):
     
     restricciones = []
     
-    # Van todos los for para agregar las restricciones
+    # Funciones
     
-    return CspProblem(variables, dominios, restricciones)
-
-def piezaDentroTablero(variable, value):
-    # Las piezas tienen que estar dentro del tablero
-    pass
-
-def piezaSuperiorIzquierda(variable, value):
-    # Por cada pieza se indique la coordenada (piso, fila, columna) de su casillero superior izquierdo.
-    pass
-
-def piezaCasilleroSalida(variable, value):
+    def piezaDentroTablero(variable, value):
+    # Las piezas tienen que estar dentro del tablero, y que toda la forma este dentro del tablero
+    
+        piso, fila, columna = value
+        id_pieza = variable
+        forma = [pieza[1] for pieza in piezas if pieza[0] == id_pieza]
+        
+        pieza_forma = FORMAS[forma]
+        
+        for parte in pieza_forma:
+            
+            nueva_x, nueva_y = parte[0] + fila, parte[1] + columna
+            
+            if not (0 < nueva_x < fila and 0 < nueva_y < columna):
+                return False
+        
+        return True
+    
+    def piezaCasilleroSalida(variable, value):
     # No debe haber ninguna pieza en el casillero de salida.
-    pass
+        piso, fila, columna = value
+        id_pieza = variable
+        forma = [pieza[1] for pieza in piezas if pieza[0] == id_pieza]
+        
+        pieza_forma = FORMAS[forma]
+        
+        for parte in pieza_forma:
+            
+            nueva_x, nueva_y = parte[0] + fila, parte[1] + columna
+            
+            if (piso == salida[0] and nueva_x == salida[1] and nueva_y == salida[2]):
+                return False
 
-def piezaSuperpuesta(variable, value):
-    # Las piezas claramente no deben estar superpuestas entre si.
-    pass
-
-def doblePiezasPisos(variable, value):
+        return True
+    
+    def doblePiezasPisos(variables, values):
     # Ningún piso tiene que tener más del doble de piezas que ningún otro piso 
     # (por ejemplo, si un piso tiene 2 piezas, otro piso puede tener 4 piezas, 
     # pero ya no puede tener 8 piezas).
+    
+        cont_pisos = [0] * pisos #Inicializamos el contador en 0
+    
+        for i in range(0,pisos):
+            for value in values:
+                if value[0] == i:
+                    cont_pisos[i] += 1
+        
+        for piso in cont_pisos - 1:
+            for piso2 in cont_pisos:
+                if not piso <= piso2 * 2:
+                    return False
+        return True
+    
+    # Van todos los for para agregar las restricciones
+    
+    for variable in variables:
+        restricciones.append((variable, piezaDentroTablero))
+        restricciones.append((variable, piezaCasilleroSalida))
+        
+    restricciones.append((variables, doblePiezasPisos))
+        
+    
+    return CspProblem(variables, dominios, restricciones)
+
+
+def piezaSuperpuesta(variable, value):
+    # Las piezas claramente no deben estar superpuestas entre si.
     pass
 
 def casillerosPiezasPisos(variable, value):
